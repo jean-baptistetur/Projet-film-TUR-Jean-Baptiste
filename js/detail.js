14aee51f85e3ca7c2735d8e611966c9dcbb8518c
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2026-03-25 12:18:20 +0100

    Ajout de l'API et affichage des films

--- js/detail.js
+++ js/detail.js
@@ -3,13 +3,19 @@ const app = document.getElementById("app")
 const params = new URLSearchParams(window.location.search)
 const id = params.get("id")
 
-if(!id){
-  app.innerHTML = "<p>Erreur : pas d'id</p>"
+const movie = getMovie(id)
+
+if(!movie){
+  app.innerHTML = "<p>Film introuvable</p>"
 }else{
   app.innerHTML = `
-    <div class="card">
-      <h2>Film ${id}</h2>
-      <p>Une description rapide du film.</p>
+    <div class="card" style="max-width:400px;margin:auto;">
+      <img src="${movie.poster}">
+      <div class="card-body">
+        <h2>${movie.title}</h2>
+        <p>${movie.date}</p>
+        <p>Note : ${movie.rating}%</p>
+      </div>
     </div>
   `
 }
